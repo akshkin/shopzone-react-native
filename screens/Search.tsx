bebca985882aa69b430fd95741c1de3f7346b4f4
akshkin <107682289+akshkin@users.noreply.github.com>
2023-07-26 10:44:42 +0200

    Add backspace icon on search bar

--- screens/Search.tsx
+++ screens/Search.tsx
@@ -1,13 +1,15 @@
-import React, { useState } from "react";
+import React, { useEffect, useLayoutEffect, useState } from "react";
 import { FlatList, StyleSheet, Text, TextInput, View } from "react-native";
 import { fetchProductsBySearch } from "../api";
 import { ProductType } from "../types";
 import ProductCard from "../components/ProductCard";
 import LoadingOverlay from "../components/LoadingOverlay";
+import IconButton from "../components/IconButton";
 
 function Search() {
   const [search, setSearch] = useState("");
   const [loading, setLoading] = useState(false);
+  const [submitted, setSubmitted] = useState(false);
   const [products, setProducts] = useState<ProductType[]>([]);
 
   async function handleSubmit() {
@@ -19,16 +21,29 @@ function Search() {
       console.log(error);
     }
     setLoading(false);
+    setSubmitted(true);
   }
+
+  useLayoutEffect(() => {
+    setSubmitted(false);
+  }, []);
 
   return (
     <View>
-      <TextInput
-        value={search}
-        style={styles.textInput}
-        onChangeText={(value) => setSearch(value)}
-        onEndEditing={handleSubmit}
-      />
+      <View style={styles.searchBar}>
+        <TextInput
+          value={search}
+          style={styles.textInput}
+          onChangeText={(value) => setSearch(value)}
+          onEndEditing={handleSubmit}
+        />
+        <IconButton
+          size={30}
+          style={styles.clearButton}
+          icon="backspace-outline"
+          onPress={() => setSearch("")}
+        />
+      </View>
       {loading ? (
         <LoadingOverlay />
       ) : products.length > 0 ? (
@@ -38,11 +53,12 @@ function Search() {
           renderItem={({ item }) => <ProductCard product={item} />}
           numColumns={2}
         />
-      ) : (
+      ) : null}
+      {!products.length && submitted ? (
         <Text style={styles.text}>
           No results found. Please try another search!
         </Text>
-      )}
+      ) : null}
     </View>
   );
 }
@@ -54,13 +70,28 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     borderWidth: 1,
     borderColor: "#6b6969",
-    margin: 8,
     fontSize: 16,
     padding: 4,
     paddingLeft: 16,
+    width: "85%",
+    borderRightWidth: 0,
   },
   text: {
     textAlign: "center",
     fontSize: 16,
+  },
+  searchBar: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    marginVertical: 8,
+  },
+  clearButton: {
+    backgroundColor: "#fff",
+    height: "100%",
+    width: "10%",
+    borderWidth: 1,
+    borderLeftWidth: 0,
+    borderColor: "#6b6969",
   },
 });
